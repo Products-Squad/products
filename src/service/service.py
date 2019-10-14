7b948991db86adf619dcaa31bedb09c086855601
vivianzhhh <zh1272@nyu.edu>
2019-10-14 09:46:15 -0400

    create api for deleting

--- src/service/service.py
+++ src/service/service.py
@@ -62,7 +62,15 @@ def list_products():
 
 
 # TODO: Implement delete product API, followed by story #5
-
+@app.route('/products/<id>', method = ['DELETE'])
+def delete_products(id):
+    """delete a product by id"""
+    app.logger.info('Request to delete product with the id provided')
+    product = Product.find_by_id(id)
+    if product:
+        product.delete()
+    return make_response('',status.HTTP_204_NO_CONTENT)
+    
 # TODO: Implement list product API, followed by story #6
 
 # TODO: Implement query product API, followed by story #7
