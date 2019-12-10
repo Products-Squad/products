c4547d7d6e2684e9f8f5c5f88b56a62bcc882fd7
LinerSu <skysoon007@gmail.com>
2019-12-10 11:58:29 -0500

    Revised test URL

--- features/environment.py
+++ features/environment.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from service.service import get_apikey_for_behave
 
 WAIT_SECONDS = 120
-BASE_URL = os.getenv('BASE_URL', 'http://localhost:5000')
+BASE_URL = os.getenv('BASE_URL', 'https://nyu-product-service-f19.mybluemix.net/')
 
 def before_all(context):
     """ Executed once before all tests """
